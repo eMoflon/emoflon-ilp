f5b460fbfae2912503ea36a3ae1e67d7c8be88af
Luise Mai <luise.mai@stud.tu-darmstadt.de>
2023-07-10 16:01:32 +0200

    Adds check if nested functions only contain linear functions for linear functions

--- org.emoflon.ilp/src/org/emoflon/ilp/LinearFunction.java
+++ org.emoflon.ilp/src/org/emoflon/ilp/LinearFunction.java
@@ -7,10 +7,14 @@ public class LinearFunction extends Function {
 
 	public LinearFunction(List<Term> terms, List<Constant> constantTerms, List<WeightedFunction> nestedFunctions) {
 		if (terms.stream().anyMatch(QuadraticTerm.class::isInstance)) {
-			throw new IllegalArgumentException("A linear function is not allowed to contain quadratic terms!");
+			throw new IllegalArgumentException("A linear function is not allowed to contain any quadratic terms!");
 		}
 		this.terms = terms;
 		this.constantTerms = constantTerms;
+		if (nestedFunctions.stream().anyMatch(nested -> !(nested.function() instanceof LinearFunction))) {
+			throw new IllegalArgumentException(
+					"A linear function is not allowed to contain any quadratic nested functions!");
+		}
 		this.nestedFunctions = nestedFunctions;
 	}
 
@@ -27,28 +31,36 @@ public class LinearFunction extends Function {
 	}
 
 	@Override
+	public void addNestedFunction(WeightedFunction func) {
+		if (func.function() instanceof LinearFunction) {
+			this.nestedFunctions.add(func);
+		} else {
+			throw new IllegalArgumentException(
+					"A linear function is not allowed to contain any quadratic nested functions!");
+		}
+	}
+
+	@Override
+	public void addNestedFunction(Function func, double weight) {
+		if (func instanceof LinearFunction) {
+			this.nestedFunctions.add(new WeightedFunction(func, weight));
+		} else {
+			throw new IllegalArgumentException(
+					"A linear function is not allowed to contain any quadratic nested functions!");
+		}
+	}
+
+	@Override
 	public Function expand() {
 		if (this.nestedFunctions.isEmpty()) {
 			// end of nesting, deepest level
 			return this;
 		} else {
-
-			Function expanded;
-			if (this.nestedFunctions.stream().anyMatch(nested -> nested.function() instanceof QuadraticFunction)) {
-				expanded = new QuadraticFunction();
-			} else {
-				expanded = new LinearFunction();
-			}
+			LinearFunction expanded = new LinearFunction();
 
 			for (WeightedFunction nested : this.nestedFunctions) {
 				double nestedWeight = nested.weight();
-
-				Function func;
-				if (nested.function() instanceof LinearFunction) {
-					func = ((LinearFunction) nested.function()).expand();
-				} else {
-					func = ((QuadraticFunction) nested.function()).expand();
-				}
+				Function func = ((LinearFunction) nested.function()).expand();
 
 				// add constants multiplied with weight
 				for (Constant cons : func.constantTerms) {
