da7184cb631dc8e0a4f69223be459fbb025313c2
Luise Mai <luise.mai@stud.tu-darmstadt.de>
2023-09-04 16:43:15 +0200

    Adds glpk to dependencies

---
+++ org.emoflon.ilp/src/org/emoflon/ilp/GlpkSolver.java
@@ -0,0 +1,51 @@
+package org.emoflon.ilp;
+
+//import org.gnu.glpk.GLPK;
+/*
+import org.gnu.glpk.GLPK;
+import org.gnu.glpk.glp_iocp;
+import org.gnu.glpk.glp_prob;
+
+public class GlpkSolver implements Solver {
+	
+
+	private glp_prob model;
+	private glp_iocp iocp;
+
+	@Override
+	public void buildILPProblem(Objective objective) {
+		// TODO Auto-generated method stub
+
+		// Convert Or Constraints
+
+		// Convert <, >, !=
+
+		// Convert SOS1 Constraints
+	}
+
+	@Override
+	public SolverOutput solve() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Objective updateValuesFromSolution() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void terminate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+*/
