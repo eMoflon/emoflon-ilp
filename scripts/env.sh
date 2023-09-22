fc72bb96d32aa4dd7408f1ca26811c9b53aa827e
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2023-09-22 10:05:17 +0200

    Updates Gurobi to v10.0.3

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,5 @@
 set -e
-export LD_LIBRARY_PATH=/opt/gurobi1002/linux64/lib/:/opt/ibm/ILOG/CPLEX_Studio2211/cplex/bin/x86-64_linux/
-export GUROBI_HOME=/opt/gurobi1002/linux64/
+export LD_LIBRARY_PATH=/opt/gurobi1003/linux64/lib/:/opt/ibm/ILOG/CPLEX_Studio2211/cplex/bin/x86-64_linux/
+export GUROBI_HOME=/opt/gurobi1003/linux64/
 export GRB_LICENSE_FILE=/home/mkratz/gurobi.lic
-export PATH=$PATH:/opt/gurobi1002/linux64/bin:/opt/ibm/ILOG/CPLEX_Studio2211/cplex/bin/x86-64_linux/
+export PATH=$PATH:/opt/gurobi1003/linux64/bin:/opt/ibm/ILOG/CPLEX_Studio2211/cplex/bin/x86-64_linux/
