2db22dceb2dc36f2690a852ec95bdd646b20ccd2
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2026-01-14 09:07:42 +0100

    CI: fixes test discovery

--- org.emoflon.ilp.tests/src/org/emoflon/ilp/tests/BasicTest.java
+++ org.emoflon.ilp.tests/src/org/emoflon/ilp/tests/BasicTest.java
@@ -1,8 +1,8 @@
 package org.emoflon.ilp.tests;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
